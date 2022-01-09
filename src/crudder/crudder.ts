876b490c37ecdd1a32727f2c292db34562b469a1
Jorge <jorge.rodriguez@adsmurai.com>
2022-01-09 10:46:53 +0100

    remove duplicate code

--- src/crudder/crudder.ts
+++ src/crudder/crudder.ts
@@ -5,27 +5,23 @@ interface ResourceActions<T> {
     getById: (id: string) => Promise<T | AxiosError>;
 }
 
+const executeAction = async <T>(action: () => Promise<T | AxiosError>): Promise<T | AxiosError> => {
+    try {
+        return await action();
+    } catch (error) {
+        return error as AxiosError;
+    }
+};
+
 const crudder =
     (domain: string) =>
     <T>(resource: string): ResourceActions<T> => {
         const url = `${domain}/${resource}`;
         return {
-            get: async (params?: { [key: string]: string }): Promise<T[] | T | AxiosError> => {
-                try {
-                    const response = await axios.get(url, { params });
-                    return response.data;
-                } catch (error) {
-                    return error as AxiosError;
-                }
-            },
-            getById: async (id: string): Promise<T | AxiosError> => {
-                try {
-                    const response = await axios.get(`${url}/${id}`);
-                    return response.data;
-                } catch (error) {
-                    return error as AxiosError;
-                }
-            },
+            get: async (params?: { [key: string]: string }): Promise<T[] | T | AxiosError> =>
+                executeAction<T[] | T>(async () => (await axios.get(url, { params })).data),
+            getById: async (id: string): Promise<T | AxiosError> =>
+                executeAction<T>(async () => (await axios.get(`${url}/${id}`)).data),
         };
     };
 
